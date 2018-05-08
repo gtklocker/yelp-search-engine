f2effa7ee6ef64ccb629ed167a3c1d5f9dc0441a
Kostis Karantias <karantiaskostis@gmail.com>
2018-05-08 20:15:26 +0000

    use global connection instead of resetting

--- src/main/scala/ir/Importer.scala
+++ src/main/scala/ir/Importer.scala
@@ -8,86 +8,75 @@ import org.apache.lucene.index.IndexWriter
 object Importer extends App {
   println("Starting import... to %s".format(Lucene.indexPath.toString()))
 
+  Class.forName("com.mysql.jdbc.Driver")
   val url = "jdbc:mysql://localhost:3306/yelp_db"
-  val driver = "com.mysql.jdbc.Driver"
   val username = "root"
   val password = "root"
-  var connection:Connection = _
+  var connection = DriverManager.getConnection(url, username, password)
   val writer = new IndexWriter(Lucene.directory, Lucene.writerConfig)
   indexReviewDocuments()
   indexBusinessDocuments()
+
   println(s"${writer.numDocs} document(s) indexed!")
+
+  connection.close
   writer.close
 
   println("Import done.")
   
   def indexReviewDocuments(){  
-    try {
-      Class.forName(driver)
-      connection = DriverManager.getConnection(url, username, password)
-      val statement =createStreamingStatement(connection) 
-      val query = """select business.name, text, date,useful 
-                     |from review
-                     |join business
-                     |on review.business_id = business.id
-                     """.stripMargin
-      println("Importing review documents: ")
-      val rs = statement.executeQuery(query)
-      
-      while (rs.next) {
-        val businessName = rs.getString("business.name")
-        val reviewText = rs.getString("text")
-        val date = rs.getDate("date").getTime()
-        val useful = rs.getLong("useful")
-        print(".")
-        val doc = new Document
-        doc.add(new StringField("business_name", businessName, Field.Store.YES))
-        doc.add(new TextField("review_text", reviewText, Field.Store.YES))
-        doc.add(new NumericDocValuesField("date", date))
-        doc.add(new NumericDocValuesField("useful", useful))
-        writer.addDocument(doc)
-      }
-      println("")
-    }catch {
-      case e: Exception => e.printStackTrace
+    val statement = createStreamingStatement(connection) 
+    val query = """select business.name, text, date,useful 
+                   |from review
+                   |join business
+                   |on review.business_id = business.id
+                   """.stripMargin
+    println("Importing review documents: ")
+    val rs = statement.executeQuery(query)
+    
+    while (rs.next) {
+      val businessName = rs.getString("business.name")
+      val reviewText = rs.getString("text")
+      val date = rs.getDate("date").getTime()
+      val useful = rs.getLong("useful")
+      print(".")
+      val doc = new Document
+      doc.add(new StringField("business_name", businessName, Field.Store.YES))
+      doc.add(new TextField("review_text", reviewText, Field.Store.YES))
+      doc.add(new NumericDocValuesField("date", date))
+      doc.add(new NumericDocValuesField("useful", useful))
+      writer.addDocument(doc)
     }
-    connection.close
+    println("")
   }
 
   def indexBusinessDocuments() {
-     try {
-      Class.forName(driver)
-      connection = DriverManager.getConnection(url, username, password)
-      val statement = createStreamingStatement(connection) 
-      val query = """select business_id, name, business.stars, group_concat(review.text," ") as text, latitude, longitude
-                    |from business join review
-                    |on business.id = review.business_id
-                    |group by business_id
-                    |having count(*)>= 100
-                    """.stripMargin
-      println("Importing business documents: ")
-      val rs = statement.executeQuery(query)
-      while (rs.next) {
-        val businessId = rs.getString("business_id")
-        val name = rs.getString("name")
-        val review = rs.getString("text")
-        val stars = rs.getFloat("stars")
-        val latitude = rs.getFloat("latitude")
-        val longitude = rs.getFloat("longitude")
-        print(".")
-        val doc = new Document
-        doc.add(new StringField("business_id", businessId, Field.Store.YES))
-        doc.add(new StringField("business_name", name, Field.Store.YES))
-        doc.add(new FloatDocValuesField("stars",stars))
-        doc.add(new TextField("review", review, Field.Store.YES))
-        doc.add(new LatLonDocValuesField("location", latitude, longitude))
-        writer.addDocument(doc)
-      }
-      println("")
-    } catch {
-      case e: Exception => e.printStackTrace
+    val statement = createStreamingStatement(connection) 
+    val query = """select business_id, name, business.stars, group_concat(review.text," ") as text, latitude, longitude
+                  |from business join review
+                  |on business.id = review.business_id
+                  |group by business_id
+                  |having count(*)>= 100
+                  """.stripMargin
+    println("Importing business documents: ")
+    val rs = statement.executeQuery(query)
+    while (rs.next) {
+      val businessId = rs.getString("business_id")
+      val name = rs.getString("name")
+      val review = rs.getString("text")
+      val stars = rs.getFloat("stars")
+      val latitude = rs.getFloat("latitude")
+      val longitude = rs.getFloat("longitude")
+      print(".")
+      val doc = new Document
+      doc.add(new StringField("business_id", businessId, Field.Store.YES))
+      doc.add(new StringField("business_name", name, Field.Store.YES))
+      doc.add(new FloatDocValuesField("stars",stars))
+      doc.add(new TextField("review", review, Field.Store.YES))
+      doc.add(new LatLonDocValuesField("location", latitude, longitude))
+      writer.addDocument(doc)
     }
-    connection.close
+    println("")
   }
   def createStreamingStatement(connection:Connection) : Statement = {
     val statement = connection.createStatement(java.sql.ResultSet.TYPE_FORWARD_ONLY,
