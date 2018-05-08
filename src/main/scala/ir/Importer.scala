359e2ef0d422adad14fe53f404d98940746e1bc1
Kostis Karantias <karantiaskostis@gmail.com>
2018-05-08 20:16:39 +0000

    cosmetic

--- src/main/scala/ir/Importer.scala
+++ src/main/scala/ir/Importer.scala
@@ -79,10 +79,11 @@ object Importer extends App {
     println("")
   }
 
-  def createStreamingStatement(connection:Connection) : Statement = {
-    val statement = connection.createStatement(java.sql.ResultSet.TYPE_FORWARD_ONLY,
-                    java.sql.ResultSet.CONCUR_READ_ONLY)
+  def createStreamingStatement(connection: Connection): Statement = {
+    val statement =
+      connection.createStatement(java.sql.ResultSet.TYPE_FORWARD_ONLY,
+        java.sql.ResultSet.CONCUR_READ_ONLY)
     statement.setFetchSize(Integer.MIN_VALUE)
-    return statement
+    statement
   }
 }
