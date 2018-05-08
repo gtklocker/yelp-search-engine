8f3f369bb6a279f713f5488aafed253a9357c0c6
Kostis Karantias <karantiaskostis@gmail.com>
2018-05-08 13:32:27 +0000

    Implement queries for functionality 1
    TODO: switch indexing to LatLonPoint as noted in the commit

--- src/main/scala/ir/Searcher.scala
+++ src/main/scala/ir/Searcher.scala
@@ -3,22 +3,41 @@ package ir
 import org.apache.lucene.analysis.core.SimpleAnalyzer
 import org.apache.lucene.analysis.standard.StandardAnalyzer
 import org.apache.lucene.index.{DirectoryReader, IndexWriter, IndexWriterConfig, Term}
-import org.apache.lucene.search.{IndexSearcher, Query, TermQuery, TopDocs}
+import org.apache.lucene.search.{IndexSearcher, Query, TermQuery, BooleanQuery, BooleanClause, TopDocs}
 import org.apache.lucene.store.RAMDirectory
+import org.apache.lucene.document.LatLonPoint
 
 object Searcher extends App {
   val reader = DirectoryReader.open(Lucene.directory)
   val searcher = new IndexSearcher(reader)
+  val MAX_HITS = 20
+  val MAX_LOCATION_RADIUS = 50
 
-  val searchTerm = "job"
-  println(s"Searching for ${searchTerm}...")
-  val query = new TermQuery(new Term("review", searchTerm))
-  val results = searcher.search(query, 10)
+  searchRestaurantsByReview(Some("bitch"), Some(51.509865, -0.118092))
 
-  println(s"${results.totalHits} result(s).")
+  def searchRestaurantsByReview(text: Option[String], location: Option[(Double, Double)]) {
+    val queryBuilder = new BooleanQuery.Builder()
+    if (text.isDefined) {
+      queryBuilder.add(reviewContainsText(text.get), BooleanClause.Occur.SHOULD)
+    }
+    if (location.isDefined) {
+      queryBuilder.add(reviewForBusinessNearLocation(location.get), BooleanClause.Occur.SHOULD)
+    }
+    val query = queryBuilder.build()
 
-  val topResult = searcher.doc(results.scoreDocs(0).doc)
-  val review = topResult.getField("review").stringValue
-  val businessName = topResult.getField("business_name").stringValue
-  println(s"Top result: @${businessName}: ${review}.")
+    val results = searcher.search(query, MAX_HITS)
+    println(s"${results.totalHits} result(s).")
+
+    val hits = results.scoreDocs.map(scoreDoc => searcher.doc(scoreDoc.doc))
+    for (hit <- hits) {
+      val review = hit.getField("review").stringValue
+      val businessName = hit.getField("business_name").stringValue
+      println(s"Business ${businessName}: ${review.substring(0, 140)}...")
+    }
+  }
+
+  def reviewContainsText(text: String): Query =
+    new TermQuery(new Term("review", text))
+  def reviewForBusinessNearLocation(location: (Double, Double)): Query =
+    LatLonPoint.newDistanceQuery("location", location._1, location._2, MAX_LOCATION_RADIUS)
 }
