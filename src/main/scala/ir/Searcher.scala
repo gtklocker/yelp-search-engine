0cd3497d9aa7cabc4dab3a0b176556ab06c43bf1
Kostis Karantias <karantiaskostis@gmail.com>
2018-05-09 21:46:21 +0000

    ability to sort

--- src/main/scala/ir/Searcher.scala
+++ src/main/scala/ir/Searcher.scala
@@ -3,9 +3,14 @@ package ir
 import org.apache.lucene.analysis.core.SimpleAnalyzer
 import org.apache.lucene.analysis.standard.StandardAnalyzer
 import org.apache.lucene.index.{DirectoryReader, IndexWriter, IndexWriterConfig, Term}
-import org.apache.lucene.search.{IndexSearcher, Query, TermQuery, BooleanQuery, BooleanClause, TopDocs}
+import org.apache.lucene.search.{IndexSearcher, Query, TermQuery, BooleanQuery,
+BooleanClause, TopDocs, SortField, Sort}
 import org.apache.lucene.store.RAMDirectory
 import org.apache.lucene.document.{Document, LatLonPoint}
+
+class BusinessSortField(val field: String, val fieldType: SortField.Type) extends SortField(field, fieldType)
+object SortByReviewCount extends BusinessSortField("business.reviewCount", SortField.Type.INT)
+object SortByStars extends BusinessSortField("business.stars", SortField.Type.FLOAT)
 
 object Searcher extends App {
   val reader = DirectoryReader.open(Lucene.directory)
@@ -13,13 +18,14 @@ object Searcher extends App {
   val MAX_HITS = 20
   val MAX_LOCATION_RADIUS = 50
 
-  val hits = findBusinesses(Some("good"), None)
+  val hits = findBusinesses(Some("good"), None, sortedBy = Some(SortByStars))
   println(s"Got ${hits.length} business hits.")
   for (business <- hits) {
     println(s"Business ${business.name}: ${business.allReviews.substring(0, 140)}...")
   }
 
-  def findBusinesses(text: Option[String], location: Option[(Double, Double)]): Array[BusinessHit] = {
+  def findBusinesses(text: Option[String], location: Option[(Double, Double)],
+      sortedBy: Option[BusinessSortField]): Array[BusinessHit] = {
     val queryBuilder = new BooleanQuery.Builder()
     if (text.isDefined) {
       queryBuilder.add(businessHasReviewContaining(text.get), BooleanClause.Occur.SHOULD)
@@ -27,7 +33,9 @@ object Searcher extends App {
     if (location.isDefined) {
       queryBuilder.add(businessNearLocation(location.get), BooleanClause.Occur.SHOULD)
     }
-    docsForQuery(queryBuilder.build())
+
+    val query = queryBuilder.build()
+    docsForQuery(query, sortedBy)
       .map(BusinessHit.fromDocument)
   }
 
@@ -37,8 +45,11 @@ object Searcher extends App {
   def businessNearLocation(location: (Double, Double)): Query =
     LatLonPoint.newDistanceQuery("business.location", location._1, location._2, MAX_LOCATION_RADIUS)
 
-  def docsForQuery(query: Query): Array[Document] = {
-    val results = searcher.search(query, MAX_HITS)
+  def docsForQuery(query: Query, sortedBy: Option[SortField]): Array[Document] = {
+    val results = sortedBy match {
+      case Some(sortField) => searcher.search(query, MAX_HITS, new Sort(sortField))
+      case None            => searcher.search(query, MAX_HITS)
+    }
     results.scoreDocs.map(scoreDoc => searcher.doc(scoreDoc.doc))
   }
 }
