2da34af770eb81ea3d72459d6549ffac2c1a9cc9
Kostis Karantias <karantiaskostis@gmail.com>
2018-05-10 14:46:51 +0000

    no new for case classes

--- src/main/scala/ir/BusinessHit.scala
+++ src/main/scala/ir/BusinessHit.scala
@@ -5,7 +5,7 @@ import org.apache.lucene.document.Document
 case class BusinessHit(id: String, name: String, allReviews: String)
 
 object BusinessHit {
-  def fromDocument(doc: Document) = new BusinessHit(
+  def fromDocument(doc: Document) = BusinessHit(
     id = doc.getField("business.id").stringValue,
     name = doc.getField("business.name").stringValue,
     allReviews = doc.getField("business.allReviews").stringValue,
--- src/main/scala/ir/ReviewHit.scala
+++ src/main/scala/ir/ReviewHit.scala
@@ -5,7 +5,7 @@ import org.apache.lucene.document.Document
 case class ReviewHit(businessName: String, text: String)
 
 object ReviewHit {
-  def fromDocument(doc: Document) = new ReviewHit(
+  def fromDocument(doc: Document) = ReviewHit(
     businessName = doc.getField("review.business.name").stringValue,
     text = doc.getField("review.text").stringValue
   )
